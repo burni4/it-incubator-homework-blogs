2177e2a614c2f56635269e7f17cf55b511945e5d
burni4 <artemnarchyk@gmail.com>
2022-10-21 21:15:09 +0300

    new project files

---
+++ src/repositories/registered-users-repository.ts
@@ -0,0 +1,4 @@
+type registredUserType = { "login": string, "password": string }
+type registredUsersType = registredUserType[]
+
+const registredUsers: registredUsersType = [{login: "admin", password: "qwerty"}];
