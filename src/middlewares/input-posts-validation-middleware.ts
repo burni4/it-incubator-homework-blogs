2ac59ba4a7d94f0857ec50b8a55ae573e404777f
burni4 <artemnarchyk@gmail.com>
2022-10-22 21:29:35 +0300

    fix

--- src/middlewares/input-posts-validation-middleware.ts
+++ src/middlewares/input-posts-validation-middleware.ts
@@ -2,11 +2,11 @@ import {body} from "express-validator";
 
 export const postTypeValidation = [
     body('title').trim().exists({checkFalsy: true}).withMessage('The field [title] must exist')
-        .bail().isLength({max: 30}).withMessage('Name length should be max 30 symbols'),
+        .bail().isLength({max: 30}).withMessage('Title length should be max 30 symbols'),
     body('shortDescription').trim().exists({checkFalsy: true}).withMessage('The field [shortDescription] must exist')
-        .bail().isLength({max: 100}).withMessage('Name length should be max 100 symbols'),
+        .bail().isLength({max: 100}).withMessage('ShortDescription length should be max 100 symbols'),
     body('content').trim().exists({checkFalsy: true}).withMessage('The field [content] must exist')
-        .bail().isLength({max: 1000}).withMessage('Name length should be max 1000 symbols'),
+        .bail().isLength({max: 1000}).withMessage('Content length should be max 1000 symbols'),
     body('blogId').exists({checkFalsy: true}).withMessage('The field [blogId] must exist')
 ]
 
