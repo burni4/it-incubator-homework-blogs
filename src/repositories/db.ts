a75a405daa80f64fcc3f12c1bd3f9a59ef20aedb
burni4 <artemnarchyk@gmail.com>
2022-10-28 19:17:22 +0300

    add dotenv

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -23,9 +23,6 @@ export type blogType = {
 const mongoURILocalhost: string = "mongodb://0.0.0.0:27017"
 
 const mongoUri = process.env.mongoURIAtlas || mongoURILocalhost;
-
-console.log(mongoUri, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-
 export const client = new MongoClient(mongoUri)
 
 export const db = client.db("it-incubator-homework-blogs")
