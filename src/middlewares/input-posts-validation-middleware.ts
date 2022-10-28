3479fc1c69cf59a51702bde8de8c379b82bf288a
burni4 <artemnarchyk@gmail.com>
2022-10-28 21:30:02 +0300

    fix bugs

--- src/middlewares/input-posts-validation-middleware.ts
+++ src/middlewares/input-posts-validation-middleware.ts
@@ -4,17 +4,18 @@ import {blogsRepository} from "../repositories/blogs-repository";
 export const postTypeValidation = [
     body('title').trim().exists({checkFalsy: true}).withMessage('The field [title] must exist')
         .bail().isLength({max: 30}).withMessage('Title length should be max 30 symbols'),
-    body('shortDescription').trim().exists({checkFalsy: true}).withMessage('The field [shortDescription] must exist')
-        .bail().isLength({max: 100}).withMessage('ShortDescription length should be max 100 symbols'),
-    body('content').trim().exists({checkFalsy: true}).withMessage('The field [content] must exist')
-        .bail().isLength({max: 1000}).withMessage('Content length should be max 1000 symbols'),
     body('blogId').exists({checkFalsy: true}).withMessage('The field [blogId] must exist')
-        .bail().custom((value, { req }) => {
+        .bail().custom((value) => {
         if (!blogsRepository.findBlogByID(value)) {
             throw new Error('BlogID does not exist');
         }
         return true;
-    })
+    }),
+    body('shortDescription').trim().exists({checkFalsy: true}).withMessage('The field [shortDescription] must exist')
+        .bail().isLength({max: 100}).withMessage('ShortDescription length should be max 100 symbols'),
+    body('content').trim().exists({checkFalsy: true}).withMessage('The field [content] must exist')
+        .bail().isLength({max: 1000}).withMessage('Content length should be max 1000 symbols'),
+
 ]
 export const postParamsValidation = [
     param('id').exists({checkFalsy: true}).withMessage('Param [id] not found')
