70fc71d8dccd810e945d355031fa5e1cf6b858fa
burni4 <artemnarchyk@gmail.com>
2022-11-11 19:33:26 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use('/blogs', blogsRouter);
 app.use('/posts', postsRouter);
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello Homework [Blogs] from Artem Narchuk");
+    res.send("Hello Homework [Blogs] from Artem Narchuk 1");
 })
 
 
