823afe7fdd14b4bb23781d51c2183da09e8c1eff
burni4 <artemnarchyk@gmail.com>
2022-11-20 11:41:24 +0300

    add auth/login

--- src/middlewares/input-auth-validation-middleware.ts
+++ src/middlewares/input-auth-validation-middleware.ts
@@ -1,7 +1,7 @@
 import {body} from "express-validator";
 
 export const authTypeValidation = [
-    body('login').exists({checkFalsy: true}).withMessage('The field [Login] must exist'),
+    body('loginOrEmail').exists({checkFalsy: true}).withMessage('The field [Login] must exist'),
     body('password').exists({checkFalsy: true}).withMessage('The field [Password] must exist')
 ]
 
--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,14 +1,19 @@
 import {Request, Response, Router} from "express";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware"
 import {authTypeValidation} from "../middlewares/input-auth-validation-middleware";
+import {usersService} from "../domain/users-service";
 
-export const authRouter = Router({});
+export const authRouter = Router({})
 
 authRouter.post('/login',
     authTypeValidation,
     inputValidationMiddleware,
-    async (req: Request, res: Response) => {
+    async (req: Request<{},{},{loginOrEmail: string, password: string}>, res: Response) => {
 
-        res.status(201);
-
+        const checkResult: boolean = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
+            if (checkResult) {
+                    res.sendStatus(204)
+            } else {
+                    res.sendStatus(401)
+            }
 })
