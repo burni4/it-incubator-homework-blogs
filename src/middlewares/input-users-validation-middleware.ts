c49ee7f23c880f04c35cd3b192f8c0b9eb0d2de6
burni4 <artemnarchyk@gmail.com>
2022-12-04 02:19:28 +0300

    add routs registrations

--- src/middlewares/input-users-validation-middleware.ts
+++ src/middlewares/input-users-validation-middleware.ts
@@ -16,14 +16,14 @@ export const validationOfExistingUsers = async (req: Request, res: Response, nex
 
         const userByLogin = await usersService.findByLogin(req.body.login)
 
-        if (!userByLogin) {
+        if (userByLogin) {
            // messageRepository.addMessage('login','Email or login already used')
             res.sendStatus(400)
             return
         }
 
         const userByEmail = await usersService.findByEmail(req.body.email)
-        if (!userByEmail) {
+        if (userByEmail) {
             //messageRepository.addMessage('email','Email or login already used')
             res.sendStatus(400)
             return
